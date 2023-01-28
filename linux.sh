ed2bd44b53ba49bfaaa679a166235cdad6f3c77e
TrialAndErrror <coding@trialanderrror.com>
2023-01-28 16:39:44 -0500

    Update linux to properly get user config

--- linux.sh
+++ linux.sh
@@ -26,10 +26,7 @@ echo -e "\n\n${GREEN}Installing Formatters: Black and Prettier${END}"
 nvim --headless -c 'MasonInstall black prettier' -c q
 
 echo -e "\n\n${GREEN}Writing Config Files...${END}"
-sudo cp -r $root/custom_config/core ~/.config/nvim/lua/
-sudo cp -r $root/custom_config/user ~/.config/nvim/lua/
-
-echo -e "\n\n${GREEN}Installing Instant...${END}"
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+sudo cp -r $root/custom_config/core ~/.config/nvim/lua/core
+sudo cp -r $root/custom_config/user ~/.config/nvim/lua/user
 
 echo -e "\n\n\n${GREEN}Successfully installed Astro NVim. Happy Coding!${END}"
